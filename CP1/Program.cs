b277f3eb97c3ca2a80f649b625bee4003768b4e7
jacintic <jacintiglesias@gmail.com>
2022-10-25 18:01:25 +0200

    Initial Setup comit

---
+++ CP1/Program.cs
@@ -0,0 +1,7 @@
+﻿// 1. Crear objetos repositorio
+
+// 2. Menú de opciones interactivo que se repita todo el tiempo
+
+// Gestionar excepciones si ocurren
+
+// Si se sele
---
+++ CP1/Repositories/ProductListRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP1.Repositories; 
+public class ProductListRepository {
+
+    // implementa métodos para trabajar con objectos Product
+}
